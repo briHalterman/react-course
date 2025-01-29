e2af88204aa558b5c7f3a1e45c1c1cf5c04e14d3
Brittany Halterman <halterma@gmail.com>
2025-01-29 17:30:51 -0500

    Dive deeper into React props

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,45 @@ import Button from './components/Button';
 import Heading from './components/Heading';
 
 function App() {
+  function printCheesecode() {
+    console.log('cheesecode');
+  }
+
   return (
     <div>
+      {/* <img
+        src="https://i.pinimg.com/736x/30/f2/3c/30f23c4db8e72dff05acb4fb076cd22d.jpg"
+        width="100"
+        height="100"
+        id="50"
+        // cheesecode=""
+      /> */}
+
       {/* <button>Thank you</button>
       <button>Thank you</button>
       <button>Thank you</button>
       <button>Thank you</button> */}
 
-      <Button />
-      <Button />
+      <Button
+        whenButtonIsClicked={printCheesecode}
+        label="Click Me!"
+        id="special"
+        cheesecode={'cheesy-code'}
+      />
+      <Button label="Submit" id="something-else">
+        Closing something
+      </Button>
       <h2>Hello</h2>
-      <Heading />
+      <Heading items={[1, 2, 3]} />
+      <Button label="Hello!" id="how-are-you" />
       <Button />
-      <Button />
-      <Button />
+      <Button label="Yoyo" />
       <h3>How are you</h3>
-      <Heading />
+      <Heading items={['hello', 'hi']} />
+      <Button>
+        <p>Hello</p>
+        <Heading />
+      </Button>
     </div>
   );
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,36 @@
-function Button() {
-  // code
-  return <button>Click Me!</button>;
+function Button({
+  label,
+  id,
+  WhenButtonIsClicked: whenClicked,
+  cheesecode = 'gooey',
+  children,
+}) {
+  // const label = props.label ?? 'default';
+  // const id = props.id;
+  // // const whenClicked = props.WhenButtonIsClicked;
+
+  if (whenClicked) {
+    whenClicked();
+  }
+
+  // console.log(props);
+
+  return (
+    <button id={id}>
+      {/* {label}
+      {cheesecode} */}
+      {children}
+    </button>
+  );
 }
 
-function sum(num1, num2) {
-  return num1 + num2;
-}
+// function sum(num1, num2) {
+//   return num1 + num2;
+// }
 
-sum(10, 50);
-sum(100, 1000);
+// sum(10, 50);
+// sum(100, 1000);
+
+// Button({})
 
 export default Button;
