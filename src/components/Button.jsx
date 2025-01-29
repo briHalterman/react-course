463f6c0ac88bf40d61b9cdbd9feb7cfba451e247
Brittany Halterman <halterma@gmail.com>
2025-01-29 16:43:55 -0500

    Explore the idea of components in React feat. JSX

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+function Button() {
+  // code
+  return <button>Click Me!</button>;
+}
+
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+sum(10, 50);
+sum(100, 1000);
+
+export default Button;
